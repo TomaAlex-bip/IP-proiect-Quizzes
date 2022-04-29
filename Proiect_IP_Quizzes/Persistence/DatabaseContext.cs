167b12b7bd0af68bc327978f53ea34c905f24fae
eusebiuonofrei <eusebiu-george.onofrei@student.tuiasi.ro>
2022-04-29 19:06:37 +0300

    test

--- Proiect_IP_Quizzes/Persistence/DatabaseContext.cs
+++ Proiect_IP_Quizzes/Persistence/DatabaseContext.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace Persistence
 {
     public class DatabaseContext
     {
+        private static DatabaseContext instance = null;
+
+        public static DatabaseContext GetInstance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new DatabaseContext();
+                }
+                return instance;
+            }
+        }
+
+        public static SQLiteConnection ConnectToDatabase()
+        { 
+            try 
+            { 
+                SQLiteConnection sqlite_conn;
+                sqlite_conn = new SQLiteConnection("Data Source=quizzes.db;Version=3;");
+                sqlite_conn.Open();
+            }
+
+            catch(SQLiteException)
+            {
+                return null;
+            }
+
+            return null;
     }
+   
+
 }
