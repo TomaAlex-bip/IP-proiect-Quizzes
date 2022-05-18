462fffc7a9bc12df8ebb6d998103a4592b36d4da
Toma Alexandru <toma.alex.xd@gmail.com>
2022-05-18 18:31:16 +0300

    added HashString function

--- Proiect_IP_Quizzes/Utils/Cryptography.cs
+++ Proiect_IP_Quizzes/Utils/Cryptography.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Utils
 {
     public class Cryptography
     {
+        public static string HashString(string str)
+        {
+            HashAlgorithm sha = new SHA1CryptoServiceProvider();
+            byte[] buf = new byte[str.Length];
+            for (int i = 0; i < str.Length; i++)
+                buf[i] = (byte)str[i];
+            byte[] result = sha.ComputeHash(buf);
+            return Convert.ToBase64String(result);
+        }
     }
 }
